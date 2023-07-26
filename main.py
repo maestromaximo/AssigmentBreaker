228a8fb61d0cf3ff1f701a0efafe449a6b538957
maestromaximo <alejandrogarcia2423@hotmail.com>
2023-07-26 02:28:17 -0400

    Fully functional

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Import required modules
 import os
 import tkinter as tk
 from tkinter import filedialog
@@ -7,84 +6,107 @@ from PIL import Image, ImageDraw
 import pytesseract
 from pytesseract import image_to_string
 from pdf2image import convert_from_path
+import re
 
 # Set path to the Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = "C:/Users/aleja/AppData/Local/Programs/Tesseract-OCR/tesseract.exe"
 
-# Function to convert PDF pages to images
+# Set this to True to enable hyper debug mode
+HyperDebugMode = False
+
 def extract_pages_as_images(pdf_path):
     return convert_from_path(pdf_path)
 
-# Function to read the question number using OCR
-def detect_question_number(image, debug=False):
-    # Define the crop area (top-right corner of the page)
+def detect_question_number(image, i, debug=False):
     left = image.width - 120
     top = 0
     right = image.width
     bottom = 120
 
-    # Crop the image
     cropped = image.crop((left, top, right, bottom))
-
-    # If debug mode is enabled, save the cropped image and the original image with a rectangle showing the cropped area
     if debug:
         draw = ImageDraw.Draw(image)
         draw.rectangle(((left, top), (right, bottom)), outline="red")
-        image.save('debug.png')
-        cropped.save('debug_cropped.png')
-    
-    # Attempt to recognize a number in the cropped image
-    try:
-        return int(image_to_string(cropped, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789'))
-    except ValueError:
-        # If the OCR fails to recognize a number, print a debug message and return 0
-        print("OCR didn't find a number on the page.")
-        return 0
+        image.save(f'debug_{i}.png')
+        cropped.save(f'debug_cropped_{i}.png')
+        
+    recognized_text = image_to_string(cropped, config='--psm 6 --oem 1').strip()
+    print(f"OCR recognized text on page {i}: {recognized_text}")
+    match = re.search(r'[1-9]|i', recognized_text)  # Search for the first digit between 1 and 9 or 'i'
+    if match:
+        recognized_digit = match.group()
+        if recognized_digit == 'i':
+            print(f"OCR recognized 'i' as '1' on page {i}.")
+            return 1
+        else:
+            return int(recognized_digit)
+    else:
+        print(f"OCR didn't find a number between 1 and 9 or 'i' on page {i}.")
+        return None
 
-# Function to write a subset of pages to a new PDF
+
+def detect_question_numberOld(image, i, debug=False):
+    left = image.width - 120
+    top = 0
+    right = image.width
+    bottom = 120
+
+    cropped = image.crop((left, top, right, bottom))
+    if debug:
+        draw = ImageDraw.Draw(image)
+        draw.rectangle(((left, top), (right, bottom)), outline="red")
+        image.save(f'debug_{i}.png')
+        cropped.save(f'debug_cropped_{i}.png')
+        
+    try:
+        recognized_number = int(image_to_string(cropped, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789'))
+        print(f"OCR recognized number on page {i}: {recognized_number}")
+        return recognized_number
+    except ValueError:
+        print(f"OCR didn't find a number on page {i}.")
+        return None
+
 def create_pdf_from_pages(pdf_reader, pages, output_path):
     pdf_writer = PdfWriter()
-    # Add the specified pages to the writer
     for page in pages:
         pdf_writer.add_page(pdf_reader.pages[page])
-    # Write the pages to a new PDF
     with open(output_path, "wb") as output_pdf:
         pdf_writer.write(output_pdf)
 
-# Main function to control the program flow
 def main():
-    # Create a Tk root widget and hide it (we only use the file dialog)
     root = tk.Tk()
     root.withdraw()
 
-    # Open a file dialog to select the input PDF
     file_path = filedialog.askopenfilename()
 
-    # Read the PDF and convert its pages to images
     pdf_reader = PdfReader(file_path)
     images = extract_pages_as_images(file_path)
     
-    # Initialize variables to keep track of the current assignment, question, and pages
-    last_question_number = 0
+    last_question_number = 1  # Start with question number 1
     assignment_number = 1
     pages = []
     
-    # Process each image
     for i, image in enumerate(images):
-        # Detect the question number using OCR
-        question_number = detect_question_number(image, debug=True)
-        # If the question number decreases, it means we have started a new assignment
-        if question_number < last_question_number:
-            # Write the previous assignment's pages to a new PDF
+        question_number = detect_question_number(image, i, debug=HyperDebugMode)
+        
+        if question_number is not None and question_number < last_question_number:
+            # Start of a new assignment detected
             output_path = f'AssignmentsBreak{assignment_number}/Q{last_question_number}.pdf'
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
             create_pdf_from_pages(pdf_reader, pages, output_path)
-            # Start a new assignment
             assignment_number += 1
-            pages = []
-        # Update the last question number and add the current page to the list of pages
-        last_question_number = question_number
-        pages.append(i)
+            pages = [i]  # Start the pages for the new assignment with the current page
+            last_question_number = question_number
+        elif question_number is not None:
+            # Start of a new question detected
+            output_path = f'AssignmentsBreak{assignment_number}/Q{last_question_number}.pdf'
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+            create_pdf_from_pages(pdf_reader, pages, output_path)
+            pages = [i]  # Start the pages for the new question with the current page
+            last_question_number = question_number
+        else:
+            # If no new question number is detected, continue appending the pages to the current question
+            pages.append(i)
 
     # If there are any remaining pages, write them to a new PDF
     if pages:
@@ -92,9 +114,8 @@ def main():
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         create_pdf_from_pages(pdf_reader, pages, output_path)
 
-    # Start the Tk event loop (even though we don't use any widgets)
     root.mainloop()
 
-# Run the main function if the script is run directly
+
 if __name__ == "__main__":
     main()
